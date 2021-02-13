7b047e85db1e2f7bd60ef8e65332dd79d14ffb8f
Muammer Yiğit Asker <masker@etu.edu.tr>
2021-02-14 00:34:06 +0300

    14.02.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]                        // => Attribute
+    public class ProductsController : ControllerBase
+    {
+        // Gevşek bağlılık = bir bağımlılık var ama soyuta bağımlılık var.
+        // naming convention  somut referans gerekli
+        // IoC Container -- Inversion of control => bellekteki bir yer ve buraya bir tane new ProductManager, EfProductDal gibi referanslar koyayım ve ihtiyaç anında bunlar kullanılsın demek.
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)                   // ProductsController new lendiğinde IProductService implemenent i vericez.
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+        
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+
+        }
+
+
+
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,59 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<IProductService,ProductManager>();                     // bana arka planda bir referans oluþtur. yani IProductService gördüðün yerde ProductManager new le diyortuz.
+            services.AddSingleton<IProductDal, EfProductDal>();                          // ProductMnager new lwdiði anda orda da bir constructor var ve IProductDal istiyor bu durumda da Ef yi new le dedim
+
+        
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
